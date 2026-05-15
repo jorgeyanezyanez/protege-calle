b2c011663b132498aa5deffa417b710dc17f61c2
Jorge <jorge.yanez@gmail.com>
2026-05-15 15:16:35 -0400

    Agregar configuracion PWA

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="es">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
